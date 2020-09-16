3c0193fe93a9c115b554b31bf94802ebcaadbc43
cdiaz19 <cdiaz19@Cristians-MacBook-Pro.local>
2020-09-15 22:37:18 -0600

    fist draft exercise 6

--- Semana 4 - Modelos y Metricas/Semana4.py
+++ Semana 4 - Modelos y Metricas/Semana4.py
@@ -1,73 +1,111 @@
-# La variable a predecir es la variable "target" la cual tiene valores de 1 y 0. Donde 1 es que el paciente tiene un padecimiento cardiaco.
+# ANTORCHA!
 
-# Los atributos X son los siguientes:
-# age
-# sex
-# chest pain type (4 values)
-# resting blood pressure
-# serum cholestoral in mg/dl
-# fasting blood sugar > 120 mg/dl
-# resting electrocardiographic results (values 0,1,2)
-# maximum heart rate achieved
-# exercise induced angina
-# oldpeak = ST depression induced by exercise relative to rest
-# the slope of the peak exercise ST segment
-# number of major vessels (0-3) colored by flourosopy
-# thal: 3 = normal; 6 = fixed defect; 7 = reversable defect
-# El dataset contiene 303 registros de un hospital de Cleveland. Es recomendable que lea el siguiente paper para que obtenga un mejor conocimiento sobre el trasfondo del problema.
-
-# Cargamos bibliotecas importantes
-import pandas as pd
+## Cargamos bibliotecas importantes
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from datetime import datetime 
 import matplotlib.pyplot as plt
-from sklearn import svm, datasets
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import plot_confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegressionCV
 
-data = pd.read_csv("data/heart.csv")
-data.head()
+## Cargamos el dataset de proyectos
+projects_data = pd.read_csv('data/proyectos.csv')
+    
+## feature engineering (transformacion, norm, eliminar var, etc...): 5 pts
 
-# EJERCICIO - Feature Engineering
-# En esta seccion incluya transformaciones, scaling, eliminar o crear variables segun considere apropiado.
-X = data.loc[:, ~data.columns.isin(['target'])]
-y = data.target
+# correlation matrix
+corr = projects_data.corr()
+corr.style.background_gradient(cmap='plasma').set_precision(2)
+
+# Revisar si existen datos nulos
+
+# Drop columns
+projects_data = projects_data.drop('name',axis = 1)
+projects_data = projects_data.drop('usd_pledged',axis = 1)
+projects_data = projects_data.drop('country',axis = 1)
+projects_data = projects_data.drop('currency',axis = 1)
+projects_data = projects_data.drop('category',axis = 1)
+projects_data = projects_data.drop('main_category',axis = 1)
+projects_data = projects_data.drop('deadline',axis = 1)
+projects_data = projects_data.drop('launched',axis = 1)
+projects_data = projects_data.drop(projects_data.index[90135])
+
+# Remove Live State rows
+projects_without_live_state = projects_data.loc[projects_data['state'] != 'live']
+
+# create columns
+
+# categorize some columns
+
+# dummies_category = pd.get_dummies(projects_without_live_state.category, prefix="category")
+# projects_without_live_state = projects_without_live_state.drop('category',axis = 1)
+# projects_without_live_state = projects_without_live_state.join(dummies_category)
+
+# dummies_main_category = pd.get_dummies(projects_without_live_state.main_category, prefix="main_category")
+# projects_without_live_state = projects_without_live_state.drop('main_category',axis = 1)
+# projects_without_live_state = projects_without_live_state.join(dummies_main_category)
+
+# dummies_state = pd.get_dummies(projects_without_live_state.state, prefix="state")
+# projects_without_live_state = projects_without_live_state.drop('state',axis = 1)
+# projects_without_live_state = projects_without_live_state.join(dummies_state)
+
+# transformar en DATETIME
+# projects_without_live_state.deadline = [datetime.strptime(dl, '%Y-%m-%d') for dl in projects_without_live_state.deadline]
+# projects_without_live_state.launched= [datetime.strptime(dl, '%Y-%m-%d %H:%M:%S') for dl in projects_without_live_state.launched]
+
+# Eliminar Nulos
+projects_without_live_state.goal = projects_without_live_state.goal.fillna(projects_without_live_state.goal.median())
+projects_without_live_state.pledged = projects_without_live_state.pledged.fillna(projects_without_live_state.pledged.median())
+projects_without_live_state.backers = projects_without_live_state.backers.fillna(projects_without_live_state.backers.median())
+projects_without_live_state.usd_pledged_real = projects_without_live_state.usd_pledged_real.fillna(projects_without_live_state.usd_pledged_real.median())
+projects_without_live_state.usd_goal_real = projects_without_live_state.usd_goal_real.fillna(projects_without_live_state.usd_goal_real.median())
+projects_without_live_state.isnull().any()
 
 
-# EJERCICIO - Entrenar Modelo & Resampling
-# Utilize SKlearn para entrenar el modelo de logistic regression. 
-# Como ud solo tiene este dataset, recuerde utilizar la mejor tecnica de resampling.
+projects_without_live_state.state = projects_without_live_state.state.astype('category')
+projects_without_live_state.state.cat.categories.unique()
 
-cv_y = np.array((y)).reshape(-1,1)
-# Cross Validation
-model = LogisticRegression(max_iter=1000, random_state=0)
-scores = cross_val_score(model, X, cv_y.reshape(-1), cv=10)
 
-print("Exactitud de cada particion:", scores)
-print("Exactitud Promedio:", scores.mean())
+## X, Y
+X = np.array((projects_without_live_state.loc[:, ~projects_without_live_state.columns.isin(['state'])]))
+y = np.array((projects_without_live_state.state))
 
-# EJERCICIO - Metricas y Evaluacion
-# Implemente la matrix de confusion y calcule todas las metricas del notebook #4 utilizando classification_report 
-# y accuracy_score. Considere el resultado obtenido e itere sobre todo el notebook hasta sentirse satisfecho con
-# los resultados. 
+## entrenamiento de modelos: 5pts
 
-# import some data to play with
-data_X = np.array((X))
-data_y = np.array((y))
-class_names = ["Sin padecimiento", "Tiene padecimiento"]
+# KNN
+                                  
+# knn = KNeighborsClassifier(n_neighbors=3).fit(X, y)
+# y_prima = knn.predict(X.T)
 
-# Split the data into a training set and a test set
-# X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, random_state=0)
+# Naibe Bayes Gaussiano
+# NB Gaussiano
+model = GaussianNB()
 
-classifier = LogisticRegressionCV(max_iter=1000, cv=10, random_state=0).fit(data_X, data_y)
+# 10-k-fold CV
+scores = cross_val_score(model, X, y, cv=10)
+print("CV:",scores)
+print("Accuracy:",scores.mean())
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+model = GaussianNB()
+y_prima = model.fit(X_train, y_train).predict(X_test)
+
+print("Accuracy:",accuracy_score(y_test, y_prima))
+
+## model selection: uso de metricas para comprar modelos y matrices de confusion: 5 pts
+
+class_names = ['canceled', 'failed', 'successful', 'suspended', 'undefined']
+classifier = LogisticRegressionCV(max_iter=1000, cv=10, random_state=0).fit(X, y)
 np.set_printoptions(precision=2)
 
-# # # Plot non-normalized confusion matrix
-disp = plot_confusion_matrix(classifier, data_X, data_y,
+disp = plot_confusion_matrix(classifier,X, y,
                              display_labels=class_names,
                              cmap=plt.cm.Blues,
                              normalize=None)
@@ -80,6 +118,4 @@ disp = plot_confusion_matrix(classifier, X_test, y_test,
                              normalize=None)
 plt.show()
 
-y_prima = classifier.predict(data_X)
-print(classification_report(data_y, y_prima, target_names=class_names))
-print("Accurancy_score:", accuracy_score(data_y, y_prima))
+## considere crear un resumen al final donde se puedan comparar las metricas "taco-a-taco", para que quede bien explicado cual modelo es el mejor.
---
+++ Semana 6 - Bayes/Semana6.py
@@ -0,0 +1,20 @@
+# Cargamos bibliotecas importantes
+import pandas as pd
+
+# Cargamos el dataset de proyectos
+proyectos_data = pd.read_csv('data/proyectos.csv')
+
+
+# feature engineering (transformacion, norm, eliminar var, etc...): 5 pts
+
+
+# entrenamiento de modelos: 5pts
+
+
+# model selection: uso de metricas para comprar modelos y matrices de confusion: 5 pts
+
+
+# considere crear un resumen al final donde se puedan comparar las metricas "taco-a-taco" 
+
+
+# para que quede bien explicado cual modelo es el mejor.
